3ed0db0e4b150e2459cd377db4b9de8828eb26aa
李尚 <hzlishang@corp.netease.com>
2019-03-23 10:50:59 +0800

    asdf

---
+++ app/src/main/java/com/snail/roguekiller/jni/NdkCrash.java
@@ -0,0 +1,12 @@
+package com.snail.roguekiller.jni;
+
+/**
+ * Author: snail
+ * Data: 2019/3/23.
+ * Des:
+ * version:
+ */
+public class NdkCrash {
+    public static native void nativecrash();
+
+}
