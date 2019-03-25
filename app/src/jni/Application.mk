be9051c96a8a33ee12b6ba9e7630c202eb4ed814
李尚 <hzlishang@corp.netease.com>
2019-03-25 11:12:37 +0800

    阿斯顿发的违法

--- app/src/jni/Application.mk
+++ app/src/jni/Application.mk
@@ -1,5 +1,5 @@
 
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions -std=c++0x
-APP_ABI := armeabi-v7a arm64-v8a armeabi
+APP_ABI := armeabi-v7a arm64-v8a armeabi x86 x86_64
 APP_PLATFORM := android-18
---
+++ app/src/obj/local/x86_64/objs/JNISample2/com_snail_roguekiller_jni_NdkCrash2.o.d
@@ -0,0 +1,2 @@
+/Users/personal/prj/Github/RogueKiller/app/src/obj/local/x86_64/objs/JNISample2/com_snail_roguekiller_jni_NdkCrash2.o: \
+  /Users/personal/prj/Github/RogueKiller/app/src/jni/com_snail_roguekiller_jni_NdkCrash2.c
